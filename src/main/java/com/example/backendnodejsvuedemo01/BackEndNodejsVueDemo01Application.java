89ddde4dadf64cfb638d237aa5c5453ffdddf90a
nick <nickbarry@163.com>
2021-08-03 17:30:48 +0800

    springboot后端开发，配合vue+nodejs开发的 前端，做前后端分离开发

---
+++ src/main/java/com/example/backendnodejsvuedemo01/BackEndNodejsVueDemo01Application.java
@@ -0,0 +1,13 @@
+package com.example.backendnodejsvuedemo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndNodejsVueDemo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndNodejsVueDemo01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendnodejsvuedemo01/BackEndNodejsVueDemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendnodejsvuedemo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndNodejsVueDemo01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
